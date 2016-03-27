7955774182f093a9288d9a0cfbc3d135f06a20ee
Marc Kalo <marc.kalo@gmail.com>
2016-03-27 04:32:26 -0400

    start->prolgue nav

---
+++ devilsfoot/Assets/Scripts/prologue/PrologueGameManager.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Text))]
+public class PrologueGameManager : MonoBehaviour {
+
+    public Text exposition;
+
+    List<string> expositionList;
+
+
+	void Start()
+    {
+        expositionList = new List<string>();
+        expositionList.Add("The events at the estate sounded ... unnatural...");
+        expositionList.Add("My curiosity was piqued.");
+        expositionList.Add("I had to embark on a journey to the estate at once.");
+        StartCoroutine("UpdateExposition");
+    }
+
+    IEnumerator UpdateExposition()
+    {
+        for(int i = 0; i < expositionList.Count; i++)
+        {
+            exposition.text = expositionList[i];
+            yield return new WaitForSeconds(2);
+        }
+
+    }
+}
---
+++ devilsfoot/Assets/Scripts/start/StartGameManager.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class StartGameManager : MonoBehaviour {
+
+	public void OnGUI()
+    {
+        if(Input.anyKeyDown)
+        {
+            SceneManager.LoadScene(1);
+        }
+    }
+}
